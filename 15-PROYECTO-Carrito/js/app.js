3c83d155043c100fff784c05937b428767788421
Sergio Hidalgo <hidalgo.sergio05@gmail.com>
2025-11-13 09:53:55 -0600

    Add basic shopping cart structure and event listeners

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,1 +1,43 @@
-const holamundo = 'Hola Mundo';
+
+//const carrito = [];
+let listaCurso = [];
+
+const curso = {
+    titulo : "",
+    autor : "",
+    cantidad : 1,
+    precio : 0,
+    imgURL: ""
+};
+
+function Curso(titulo, autor, cantidad, precio, imgURL){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.cantidad = cantidad;
+    this.precio = precio;
+    this.imgURL = imgURL;
+};
+ 
+
+const listaCursosHTML = document.querySelector("#lista-cursos");
+ 
+
+inicializarEvenetsListeners();
+
+
+function inicializarEvenetsListeners() {
+
+    listaCursosHTML.addEventListener('click', addCarrito);
+
+}
+ 
+function addCarrito(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains("agregar-carrito")) {
+        console.log(e.target.parentElement.parentElement);
+    }
+}
+ 
+
+
